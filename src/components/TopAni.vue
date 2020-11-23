4e8c1ebaa8bfa42f20571eee0427d33e98f136ea
sholt20 <samuelholt123@gmail.com>
2020-11-23 05:10:53 -0500

    Start of animated header

---
+++ src/components/TopAni.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="header-ani-container">
+
+    </div>
+</template>
+
+<script>
+import { onMounted, reactive } from 'vue'
+import * as PIXI from 'pixi.js'
+export default {
+    name: 'HeaderAni',
+    setup() {
+        const state = reactive({
+            app: {},
+            blackHole: {}
+        })
+
+        function initPixi() {
+            state.app = new PIXI.Application()
+            document.getElementById("header-ani-container").appendChild(state.app.view)
+            const parent = state.app.view.parentNode;
+
+            state.app.renderer.resize(parent.clientWidth, parent.clientHeight)
+        }
+
+        onMounted(() => {
+            initPixi()
+        })
+    }
+}
+</script>
+
+<style scoped>
+#headeer-ani-container {
+    width: 100%;
+}
+</style>
